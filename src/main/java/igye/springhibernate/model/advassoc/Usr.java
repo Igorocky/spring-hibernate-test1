11667e888baeb7f7e7f787ee88af2b694148779c
Igorocky <example@mail.com>
2018-03-30 18:48:08 +0200

    @JoinColumn

--- src/main/java/igye/springhibernate/model/advassoc/Usr.java
+++ src/main/java/igye/springhibernate/model/advassoc/Usr.java
@@ -1,16 +1,18 @@
 package igye.springhibernate.model.advassoc;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 
 @Entity
 public class Usr {
     @Id
+    @GeneratedValue
     private Long id;
 
-    @OneToOne(
-            fetch = FetchType.LAZY, optional = false
-    )
-    @PrimaryKeyJoinColumn
+    @OneToOne(fetch = FetchType.LAZY)
+    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE})
+    @JoinColumn
     protected Addr shippingAddress;
 
     private String name;
